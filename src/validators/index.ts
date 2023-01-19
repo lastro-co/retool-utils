94091d3c060b9419b0fc9dd3200896705aeec782
André Vandal <me@andrevandal.dev>
2023-01-19 19:42:04 -0300

    feat: add validateInputVatNumberCustomRule

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -2,8 +2,25 @@ import validateVatNumber from "~/validators/vatNumber"
 import validateCNPJ from "~/validators/cnpj"
 import validateCPF from "~/validators/cpf"
 
+function validateInputVatNumberCustomRule (value: string){
+  const isValid = validateVatNumber(value)
+  return (value && !isValid) ? 'CPF/CNPJ inválido' : ''
+}
+
 export default {
   validateVatNumber,
   validateCNPJ,
-  validateCPF
+  validateCPF,
+  validateInputVatNumberCustomRule
+}
+
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest
+  it('validateCPF', () => {
+    expect(validateInputVatNumberCustomRule('493.853.650-10')).toBe('')
+    expect(validateInputVatNumberCustomRule('02.108.487/0001-56')).toBe('')
+    expect(validateInputVatNumberCustomRule('')).toBe('')
+    expect(validateInputVatNumberCustomRule('49385365013')).toBe('CPF/CNPJ inválido')
+  })
 }
