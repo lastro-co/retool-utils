253ef11e32a091cabdaa664c76bce70012353965
André Vandal <me@andrevandal.dev>
2023-04-25 13:43:25 -0300

    feat: export convertJSONToCSV helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,15 +1,18 @@
 import compareArraysOfObjectsWithId from "~/helpers/compareArraysOfObjectsWithId"
 import flattenObject from "~/helpers/flattenObject"
 import unflattenObject from "~/helpers/unflattenObject"
+import convertJSONToCSV from "~/helpers/convertJSONToCSV"
 
 export {
   compareArraysOfObjectsWithId,
   flattenObject,
-  unflattenObject
+  unflattenObject,
+  convertJSONToCSV
 }
 
 export default {
   compareArraysOfObjectsWithId,
   flattenObject,
-  unflattenObject
+  unflattenObject,
+  convertJSONToCSV
 }
