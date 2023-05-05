14c2c087a25ac7cd861d97e0f9e5bd4903ad8b6d
André Vandal <me@andrevandal.dev>
2023-05-04 22:45:32 -0300

    feat: handle ids in groupCycle

--- src/helpers/groupCycles.ts
+++ src/helpers/groupCycles.ts
@@ -6,12 +6,16 @@ type TableEntry = {
   description?: string;
   adjustment?: number | string;
   adjustable?: boolean;
+  cycleId?: string;
+  id?: string;
 };
 
 type Cycle = {
+  id?: string;
   initialMonth: number;
   finalMonth: number;
   baseAmounts: {
+    id?: string;
     amount: number;
     type: string;
     description?: string;
@@ -20,44 +24,59 @@ type Cycle = {
   }[];
 };
 
+function toNumber(value: number | string | undefined | null): number {
+  if (value === undefined || value === null) {
+    return 0
+  }
+  return typeof value === 'number' ? value : Number(value) || 0
+}
+
+function toBoolean(value: boolean | undefined): boolean {
+  return value || false
+}
+
 function groupCycles(tableData: TableEntry[]): Cycle[] {
-  const sortedTableData = tableData.sort((a, b) => Number(a.initialMonth) - Number(b.initialMonth))
+  const sortedTableData = tableData.sort((a, b) => toNumber(a.initialMonth) - toNumber(b.initialMonth))
 
   const cycles = sortedTableData.reduce<Cycle[]>((acc, entry) => {
     const existingCycleIndex = acc.findIndex(
-      cycle => cycle.initialMonth <= Number(entry.initialMonth) && cycle.finalMonth >= Number(entry.finalMonth)
+      cycle => (cycle.id && entry.cycleId ? cycle.id === entry.cycleId : true) && cycle.initialMonth <= toNumber(entry.initialMonth) && cycle.finalMonth >= toNumber(entry.finalMonth)
     )
 
     const entryData = {
-      amount: Number(entry.amount),
+      id: entry.id ?? undefined,
+      amount: toNumber(entry.amount),
       type: entry.type,
       description: entry.description || "",
-      adjustment: Number(entry.adjustment) || 0,
-      adjustable: entry.adjustable || false,
+      adjustment: toNumber(entry.adjustment),
+      adjustable: toBoolean(entry.adjustable),
     }
 
     if (existingCycleIndex >= 0) {
       const existingCycle = acc[existingCycleIndex]
 
-      if (existingCycle.initialMonth < Number(entry.initialMonth)) {
+      if (existingCycle.initialMonth < toNumber(entry.initialMonth)) {
         acc.splice(existingCycleIndex, 1, {
+          id: existingCycle.id,
           initialMonth: existingCycle.initialMonth,
-          finalMonth: Number(entry.initialMonth) - 1,
+          finalMonth: toNumber(entry.initialMonth) - 1,
           baseAmounts: existingCycle.baseAmounts,
         })
 
         acc.splice(existingCycleIndex + 1, 0, {
-          initialMonth: Number(entry.initialMonth),
-          finalMonth: Number(entry.finalMonth),
+          id: existingCycle.id,
+          initialMonth: toNumber(entry.initialMonth),
+          finalMonth: toNumber(entry.finalMonth),
           baseAmounts: [
             ...existingCycle.baseAmounts,
             entryData,
           ],
         })
 
-        if (existingCycle.finalMonth > Number(entry.finalMonth)) {
+        if (existingCycle.finalMonth > toNumber(entry.finalMonth)) {
           acc.splice(existingCycleIndex + 2, 0, {
-            initialMonth: Number(entry.finalMonth) + 1,
+            id: existingCycle.id,
+            initialMonth: toNumber(entry.finalMonth) + 1,
             finalMonth: existingCycle.finalMonth,
             baseAmounts: existingCycle.baseAmounts,
           })
@@ -67,8 +86,9 @@ function groupCycles(tableData: TableEntry[]): Cycle[] {
       }
     } else {
       acc.push({
-        initialMonth: Number(entry.initialMonth),
-        finalMonth: Number(entry.finalMonth),
+        id: entry.cycleId ?? undefined,
+        initialMonth: toNumber(entry.initialMonth),
+        finalMonth: toNumber(entry.finalMonth),
         baseAmounts: [entryData],
       })
     }
@@ -88,8 +108,12 @@ if (import.meta.vitest) {
     initialMonth: number | string,
     finalMonth: number | string,
     amount: number | string,
-    type: string
+    type: string,
+    id?: string,
+    cycleId?: string
   ) => ({
+    id,
+    cycleId,
     initialMonth,
     finalMonth,
     amount,
@@ -100,18 +124,25 @@ if (import.meta.vitest) {
     initialMonth: number,
     finalMonth: number,
     baseAmounts: Array<{
+      id?: string,
       amount: number;
       type: string;
-    }>
+      description?: string;
+      adjustment?: number;
+      adjustable?: boolean;
+    }>,
+    id?: string
   ) => ({
+    id,
     initialMonth,
     finalMonth,
     baseAmounts: baseAmounts.map(b => ({
+      id: b.id,
       amount: b.amount,
       type: b.type,
-      description: "",
-      adjustment: 0,
-      adjustable: false,
+      description: b.description || "",
+      adjustment: b.adjustment || 0,
+      adjustable: b.adjustable || false,
     })),
   })
 
@@ -121,14 +152,14 @@ if (import.meta.vitest) {
         testName:
           "should group base amounts with the same initialMonth and finalMonth into the same cycle",
         tableData: [
-          createEntry(1, 12, 1000, "rent"),
-          createEntry(1, 12, -500, "rent-discount"),
+          createEntry(1, 12, 1000, "rent", "base-amount-1", "cycle-1"),
+          createEntry(1, 12, -500, "rent-discount", "base-amount-2", "cycle-1"),
         ],
         expectedResult: [
           createCycle(1, 12, [
-            { amount: 1000, type: "rent" },
-            { amount: -500, type: "rent-discount" },
-          ]),
+            { id: "base-amount-1", amount: 1000, type: "rent" },
+            { id: "base-amount-2", amount: -500, type: "rent-discount" },
+          ], "cycle-1"),
         ],
       },
       {
